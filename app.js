cd78aa529511cec3cefb71ef5bc66556991fa64e
Asif2006 <ashifkhan9660951828@gmail.com>
2024-09-17 10:24:36 +0530

    animation

---
+++ app.js
@@ -0,0 +1,17 @@
+// let alltime=document.querySelectorAll('span')
+
+// alltime.forEach((item)=>{
+//   item.addEventListener('click',function(){
+// item.classList.toggle('active')
+//     })
+// })
+
+
+let animation=document.querySelectorAll('span')
+
+
+animation.forEach((item)=>{
+  item.addEventListener('click',()=>{
+item.classList.toggle('active')
+  })
+})
